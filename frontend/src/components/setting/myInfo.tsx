27da391b2a5e0aaca9c57db56c88b1e902a452b3
박수빈 <tns027@naver.com>
2024-11-08 10:18:14 +0900

    FE : 마이페이지 크루 목록 api 연결

--- frontend/src/components/setting/myInfo.tsx
+++ frontend/src/components/setting/myInfo.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import useMyInfo from '@/hooks/setting/useMyInfo';
 import { UserInfo } from '@/hooks/setting/useMyInfo';
-import { Input, Box } from '@chakra-ui/react';
-import { userInfo } from 'os';
+import { Input, Box, Button } from '@chakra-ui/react';
 
 const regions = ['서울', '부산', '대구', '인천', '광주', '대전', '울산', '제주', '경기도 남부', '경기도 북부', '강원도 남부', '강원도 북부', '충청북도', '충청남도', '전라북도', '전라남도', '경상북도', '경상남도']
 const genders = ['남성', '여성', '기타']
@@ -27,124 +26,128 @@ const MyInfo: React.FC<{ myInfo: UserInfo }> = ({ myInfo }) => {
   return (
     <Box>
     {modifiedInfo ? (
-        <form onSubmit={handleSubmit}>
-          <div style={{ marginTop: "20px"}}>
+      <form onSubmit={handleSubmit}>
+        <Box display="flex" flexDirection="row" marginTop="20px">
+          <div>
+            <label>별명</label>
+            <Input
+              type="text"
+              name="nickname"
+              placeholder={modifiedInfo.nickname}
+              onChange={handleChange}
+              color="white"
+              width="300px"
+              marginLeft="10px"
+            />
+          </div>
+          <Box marginLeft="30px">
             <label>이름</label>
             <Input
               type="text"
               name="name"
               value={modifiedInfo.name}
               onChange={handleChange}
+              color="white"
+              width="300px"
+              disabled
+              marginLeft="10px"
+            />
+          </Box>
+        </Box>
+        <div style={{ marginTop: "10px"}}>
+          <label>아이디</label>
+          <Input
+            type="email"
+            name="email"
+            value={modifiedInfo.email}
+            onChange={handleChange}
             color="white"
             width="300px"
-            disabled
             marginLeft="10px"
-            
-            />
-          </div>
-
-          <div style={{ marginTop: "20px"}}>
-            <label>별명</label>
-            <Input
-              type="text"
-              name="nickname"
-              // value={modifiedInfo.nickname}
-              placeholder={modifiedInfo.nickname}
-              onChange={handleChange}
-              color="white"
-              width="300px"
-              marginLeft="10px"
-              />
-          </div>
-
-          <div style={{ marginTop: "20px"}}>
-            <label>아이디</label>
-            <Input
-              type="email"
-              name="email"
-              value={modifiedInfo.email}
-                onChange={handleChange}
-              color="white"
-              width="300px"
-              marginLeft="10px"
-              disabled
-              />
-          </div>
-
-          <div style={{ marginTop: "20px"}}>
-            <Box display="flex" flexDirection="row" marginLeft="10px">
+            disabled
+          />
+        </div>
+        <Box 
+          display="flex" 
+          flexDirection="row" 
+          marginTop="10px"
+        >
+          <Box>
+            <Box display="flex" flexDirection="row">
               <label>지역</label>
-            <Box
-              width="70px"
-              marginLeft="10px"
-            >
-            <select 
-            name='region'
-            value={modifiedInfo.region}
-            onChange={handleChange}
-            style={{ color: 'black', background: 'white', borderColor: 'black', padding: '0.5rem' }}>
-                <option>전라남도</option>
-                {regions.map((region, index) => {
-                if (region !== myInfo.region) {
-                    return <option key={index} value={region}>{region}</option>;
-                }
-                })}
-            </select>
-            </Box>
+              <Box
+                width="70px"
+                marginLeft="10px"
+              >
+                <select 
+                  name='region'
+                  value={modifiedInfo.region}
+                  onChange={handleChange}
+                  style={{ color: 'black', background: 'white', borderColor: 'black', padding: '0.5rem' }}
+                >
+                  <option>전라남도</option>
+                  {regions.map((region, index) => {
+                    if (region !== myInfo.region) {
+                        return <option key={index} value={region}>{region}</option>;
+                    }
+                  })}
+                </select>
               </Box>
-          </div>
-
-          <div style={{ marginTop: "20px"}}>
+            </Box>
+          </Box>
+          <Box marginLeft="100px">
             <label>성별</label>
             <select 
               name='gender'
               value={modifiedInfo.gender}
               onChange={handleChange}
-              style={{ color: 'black', background: 'white', borderColor: 'black', padding: '0.5rem',  marginLeft:"10px" }}>
-                <option>남성</option>
-                {genders.map((gender, index) => {
-                  if (gender !== myInfo.region) {
-                    return <option key={index} value={gender}>{gender}</option>;
-                  }
-                })}
+              style={{ color: 'black', background: 'white', borderColor: 'black', padding: '0.5rem',  marginLeft:"10px" }}
+            >
+              <option>남성</option>
+              {genders.map((gender, index) => {
+                if (gender !== myInfo.region) {
+                  return <option key={index} value={gender}>{gender}</option>;
+                }
+              })}
             </select>
-
-          </div>
-
-          <div style={{ marginTop: "20px"}}>
+          </Box>
+        </Box>      
+        <Box display="flex" flexDirection="row" marginTop= "10px">
+          <Box>
             <label>포지션</label>
               <select 
                 name='position'
                 value={modifiedInfo.position}
                 onChange={handleChange}
-              style={{ color: 'black', background: 'white', borderColor: 'black', padding: '0.5rem',  marginLeft:"10px"}}>
-                  <option>건반</option>
-                  {positions.map((position, index) => {
+                style={{ color: 'black', background: 'white', borderColor: 'black', padding: '0.5rem',  marginLeft:"10px"}}
+              >
+                <option>건반</option>
+                {positions.map((position, index) => {
                   if (position !== myInfo.region) {
-                      return <option key={index} value={position}>{position}</option>;
+                    return <option key={index} value={position}>{position}</option>;
                   }
-                  })}
+                })}
               </select>
-
-          </div>
-          <div style={{ marginTop: "20px"}}>
-            <label>장르</label>
-              <select 
-                name='genre'
-                value={modifiedInfo.genre}
-                onChange={handleChange}
-              style={{ color: 'black', background: 'white', borderColor: 'black', padding: '0.5rem',  marginLeft:"10px"}}>
+            </Box>
+            <Box  marginLeft="30px">
+              <label>장르</label>
+                <select 
+                  name='genre'
+                  value={modifiedInfo.genre}
+                  onChange={handleChange}
+                  style={{ color: 'black', background: 'white', borderColor: 'black', padding: '0.5rem',  marginLeft:"10px"}}
+                >
                   <option>건반</option>
                   {genres.map((genre, index) => {
-                  if (genre !== myInfo.region) {
+                    if (genre !== myInfo.region) {
                       return <option key={index} value={genre}>{genre}</option>;
-                  }
+                    }
                   })}
-              </select>
+                </select>
+            </Box>
+          </Box>
 
-          </div>
-
-          <div style={{ marginTop: "20px"}}>
+          <div style={{ marginTop: "10px"}}>
             <label>생일</label>
             <Input
               type="date"
@@ -159,7 +162,7 @@ const MyInfo: React.FC<{ myInfo: UserInfo }> = ({ myInfo }) => {
               />
           </div>
 
-          <div style={{ marginTop: "20px"}}>
+          <div style={{ marginTop: "10px"}}>
             <label>프로필 이미지</label>
             <Input
               type="file"
@@ -170,17 +173,16 @@ const MyInfo: React.FC<{ myInfo: UserInfo }> = ({ myInfo }) => {
                   if (file) {
                       // setFormData(prevData => ({ ...prevData, profileImage: file }));
                       // setChangedFields(prev => ({ ...prev, profileImage: true }));
-                    }
+                  }
               }}
               background="white"
               color="black"
               width="300px"
-              />
+            />
           </div>
-
-          <button type="submit">수정</button>
+          <Button type="submit">수정</Button>
         </form>
-      ) : null}
+    ) : null}
   </Box>
   );
 };
--- frontend/src/hooks/setting/useMyCrew.ts
+++ frontend/src/hooks/setting/useMyCrew.ts
@@ -1,17 +1,41 @@
+import axios from "axios";
+import { useState } from "react";
+
 interface Crew {
-    id: number;
+    crewSeq: number;
+    nickname: string;
+    profileImage: string;
   }
 
 const useMyCrew = () => {
+    const [ crewData, setCrewData ] = useState<Crew[]>([]);
 
     const getMyCrews = async (crewsInfo: Crew[]) => {
-        crewsInfo.map((crewSeq, index) => {
-            console.log(crewSeq)
-        })
-    }
+        const API_URL = import.meta.env.VITE_API_URL;
+        const storedToken = localStorage.getItem('jwtToken');
+        const fetchedCrews: Crew[] = [];
+        for (const crewSeq of crewsInfo) {
+            try {
+                const response = await axios.get(
+                    `${API_URL}/api/crew/${crewSeq}`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${storedToken}` 
+                        },
+                    }
+                )
+                fetchedCrews.push(response.data);
+            } catch(error) {
+                console.warn(error)
+            }
+        }
+        setCrewData(fetchedCrews);
+    };
+
     return {
-        getMyCrews
+        getMyCrews,
+        crewData
     }
-}
+};
 
 export default useMyCrew;
