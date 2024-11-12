9e9a0fec0b73293f135c287e70cd86e14279c03b
구현우 <kjklovekhw@naver.com>
2024-11-12 17:31:24 +0900

    Frontend community hw

--- frontend/src/hooks/community/useCommunityMain.ts
+++ frontend/src/hooks/community/useCommunityMain.ts
@@ -3,59 +3,60 @@ import axios from "axios";
 import { ArticleItem } from "@/configs/community/articleItem";
 
 const useCommunityMain = () => {
-    const [feedState, setFeedState] = useState<boolean>(true);
-    const [ changeHeader, setChangeHeader ] = useState<boolean>(true);
-    const [ myFeedArticleItems, setMyFeedArticleItems ] = useState<ArticleItem[]>([]);
+  const [feedState, setFeedState] = useState<boolean>(true);
+  const [changeHeader, setChangeHeader] = useState<boolean>(true);
+  const [myFeedArticleItems, setMyFeedArticleItems] = useState<ArticleItem[]>(
+    []
+  );
 
+  const API_URL = import.meta.env.VITE_API_URL;
 
-    const API_URL =import.meta.env.VITE_API_URL;
+  const goMusicMainFeed = () => {
+    setFeedState(false);
+  };
 
-    const goMusicMainFeed = () => {
-        setFeedState(false);
-    };
+  const goMainFeed = () => {
+    setFeedState(true);
+  };
 
-    const goMainFeed = () => {
-        setFeedState(true);
-    };
+  const handleChangeHeader = () => {
+    setChangeHeader(false);
+  };
 
-    const handleChangeHeader = () => {
-        setChangeHeader(false);
+  // 게시물 목록 가져오기
+  const getArticleList = async (artistSeq: Number) => {
+    const storedToken = localStorage.getItem("jwtToken");
+
+    if (!storedToken || artistSeq === null) {
+      console.error("No token or invalid artistSeq");
+      return;
     }
 
-    // 게시물 목록 가져오기
-    const getArticleList = async (artistSeq: Number) => {
-        const storedToken = localStorage.getItem('jwtToken');
-
-        if (!storedToken || artistSeq === null) {
-            console.error('No token or invalid artistSeq');
-            return;
+    try {
+      const response = await axios.get(
+        `${API_URL}/api/boards/artists/${artistSeq}`,
+        {
+          headers: {
+            Authorization: `Bearer ${storedToken}`,
+          },
         }
-
-        try {
-            const response = await axios.get(`${API_URL}/api/boards/artists/${artistSeq}`,
-            {
-                headers: {
-                    Authorization: `Bearer ${storedToken}`
-                },
-            })
-        setMyFeedArticleItems([response.data]);
-        } catch(error) {
-          console.error(error)
-        }
-
+      );
+      console.log(response);
+      setMyFeedArticleItems([response.data]);
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-   
-
-    return {
-        feedState,
-        changeHeader,
-        myFeedArticleItems,
-        goMusicMainFeed,
-        goMainFeed,
-        handleChangeHeader,
-        getArticleList
-    };
+  return {
+    feedState,
+    changeHeader,
+    myFeedArticleItems,
+    goMusicMainFeed,
+    goMainFeed,
+    handleChangeHeader,
+    getArticleList,
+  };
 };
 
 export default useCommunityMain;
--- frontend/src/pages/community/community-create-view.tsx
+++ frontend/src/pages/community/community-create-view.tsx
@@ -1,23 +1,23 @@
-import React, { useEffect } from 'react';
-import InputContainer from '@/components/community/input-container';
-import Header from '@/components/community/header';
-import CrewHeader from '@/components/community/crew-header';
-import Container from '@/components/community/container';
-import useHeaderStore from '@/stores/headerStore';
+import React, { useEffect } from "react";
+import InputContainer from "@/components/community/input-container";
+import Header from "@/components/community/header";
+import CrewHeader from "@/components/community/crew-header";
+import Container from "@/components/community/container";
+import useHeaderStore from "@/stores/headerStore";
 
 const CommunityCreateView: React.FC = () => {
-  const { openUserHeader } = useHeaderStore(state => state);
+  const { openUserHeader } = useHeaderStore((state) => state);
 
   useEffect(() => {
-    console.log('글쓰기 페이지에서 헤더', openUserHeader)
-  }, [openUserHeader])
+    console.log("글쓰기 페이지에서 헤더", openUserHeader);
+  }, [openUserHeader]);
 
   return (
     <>
       {openUserHeader ? <Header /> : <CrewHeader />}
       <Container>
         <InputContainer />
-      </Container>  
+      </Container>
     </>
   );
 };
