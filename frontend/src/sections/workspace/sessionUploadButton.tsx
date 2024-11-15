7040698fa5468fd5be66a6e284f2a52caec062e2
김송희 <kkbuh2@gmail.com>
2024-11-15 12:07:44 +0900

    [FE] 워크스페이스 디테일 페이지 UI, 재생바 등

--- frontend/src/sections/workspace/sessionUploadButton.tsx
+++ frontend/src/sections/workspace/sessionUploadButton.tsx
@@ -88,11 +88,23 @@ export default function SessionUploadButton({
     <PopoverRoot>
       <PopoverTrigger asChild>
         <Button
-          size="sm"
-          variant="outline"
-          fontFamily="MiceGothic"
-          fontSize={13}
-          color="white"
+          // size="sm"
+          // variant="outline"
+          // fontFamily="MiceGothic"
+          // fontSize={13}
+          // color="white"
+          bg="blackAlpha.900" // 검은 배경
+          color="white" // 텍스트 색상
+          border="2px solid" // 테두리 두께
+          borderColor="purple.500" // 보라색 테두리
+          borderRadius="md" // 모서리 둥글게
+          _hover={{ bg: "purple.700" }} // 호버 효과
+          _active={{ bg: "purple.800" }} // 클릭 효과
+          paddingX="4"
+          paddingY="2"
+          width="110px"
+          height="42px"
+          fontWeight="bold"
         >
           세션 추가
         </Button>
